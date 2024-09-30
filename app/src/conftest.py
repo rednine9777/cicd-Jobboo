e8c9f44772872b86ea303dc73c31bb63b7079170
rednine9777 <rednine9777@gmail.com>
2024-09-30 18:29:59 +0900

    alksdm

--- app/src/conftest.py
+++ app/src/conftest.py
@@ -2,4 +2,4 @@ import sys
 import os
 
 # 필요한 경로를 추가합니다.
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'main/goo')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
