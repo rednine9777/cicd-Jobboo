5d14448be462a6cf0a7520f0b6dfd83513272108
rednine9777 <rednine9777@gmail.com>
2024-10-01 00:03:26 +0900

    alskdlkajsd

--- app/src/conftest.py
+++ app/src/conftest.py
@@ -1,13 +1,27 @@
 import pytest
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from app.models import Base  # 정확한 경로로 수정
+import sys
+import os
 
-# DB URL 설정
+# app 디렉토리의 경로를 Python 경로에 추가
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+print("Current sys.path:", sys.path)  # 추가된 경로 출력
+
+# models 파일에서 Base를 가져옵니다.
+from app.models import Base  # 정확한 경로로 수정 필요
+
+# 환경 변수 로드
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '../../../config/.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+# async_session fixture 정의
 @pytest.fixture
 async def async_session():
-    engine = create_async_engine("mysql+aiomysql://user:password@localhost/test_db", echo=True)
-    async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    db_url = f"mysql+aiomysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
+    engine = create_async_engine(db_url, echo=True)
+    async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
     # 테이블 생성
     async with engine.begin() as conn:
