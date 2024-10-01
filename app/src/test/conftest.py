92cd2eeafe77d97e0191aeb8b90245c9fb333dac
rednine9777 <rednine9777@gmail.com>
2024-10-01 14:18:49 +0900

    aslkdjalksjd

--- app/src/test/conftest.py
+++ app/src/test/conftest.py
@@ -1,15 +1,12 @@
 import pytest
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-import sys
 import os
 from dotenv import load_dotenv
-from models import Base
 
 dotenv_path = os.getenv('DOTENV_PATH')
 load_dotenv(dotenv_path=dotenv_path)
 
-# async_session fixture 정의
 @pytest.fixture
 async def async_session():
     db_url = f"mysql+aiomysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
@@ -17,15 +14,11 @@ async def async_session():
     engine = create_async_engine(db_url, echo=True)
     async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-    # 세션 확인
+    # 세션 생성 및 제공
     print("세션 생성 시도 중...")
-    
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        print("테이블 생성 완료")
-    
     async with async_session_maker() as session:
         print("세션이 생성되었습니다:", session)
         yield session
 
+    # 세션 종료 후 리소스 정리
     await engine.dispose()
